61e6ede0812796209f18ee2077db855ccb2bac6c
ota42y <ota42y@gmail.com>
2015-10-20 22:37:00 +0900

    add Members

--- member.go
+++ member.go
@@ -1,14 +1,25 @@
 package gochatwork
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/url"
 )
 
+// Members return rooms/room_id/members response by []Account
+func (c *Client) Members(roomID int64) ([]Account, error) {
+	var accounts []Account
 
-// MembersRaw return rooms/room_id/members and response by []byte
+	b, err := c.MembersRaw(roomID)
+	if err != nil {
+		return accounts, err
+	}
+
+	err = json.Unmarshal(b, &accounts)
+	return accounts, err
+}
+
+// MembersRaw return rooms/room_id/members response by []byte
 func (c *Client) MembersRaw(roomID int64) ([]byte, error) {
 	return c.connection.Get(fmt.Sprintf("rooms/%d/members", roomID), url.Values{}, c.config)
 }
-
-
--- member_test.go
+++ member_test.go
@@ -11,13 +11,72 @@ func TestMembers(t *testing.T) {
 	client := New(testToken)
 
 	Convey("correct", t, func() {
+		correctJSON := `
+[
+  {
+	"account_id":1,
+	"role":"string_2",
+	"name":"string_3",
+	"chatwork_id":"string_4",
+	"organization_id":5,
+	"organization_name":"string_6",
+	"department":"string_7",
+	"avatar_image_url":"https://string_8"
+  },{
+	"account_id":9,
+	"role":"string_10",
+	"name":"string_11",
+	"chatwork_id":"string_12",
+	"organization_id":13,
+	"organization_name":"string_14",
+	"department":"string_15",
+	"avatar_image_url":"https://string_16"
+  }
+]
+`
+		Convey("Members", func() {
+			stub := &stubHTTP{}
+			stub.GetByte = []byte(correctJSON)
+			client.connection = stub
+
+			accounts, err := client.Members(42)
+
+			So(stub.GetCount, ShouldEqual, 1)
+			So(stub.GetEndPoint, ShouldEqual, "rooms/42/members")
+
+			So(err, ShouldBeNil)
+			So(len(accounts), ShouldEqual, 2)
+
+			v := &TestValue{}
+			v.Count = 1
+			account := accounts[0]
+			So(account.AccountID, ShouldEqual, v.GetInt64())
+			So(account.Role, ShouldEqual, v.GetString())
+			So(account.Name, ShouldEqual, v.GetString())
+			So(account.ChatworkID, ShouldEqual, v.GetString())
+			So(account.OrganizationID, ShouldEqual, v.GetInt64())
+			So(account.OrganizationName, ShouldEqual, v.GetString())
+			So(account.Department, ShouldEqual, v.GetString())
+			So(account.AvatarImageURL, ShouldEqual, "https://"+v.GetString())
+
+			account = accounts[1]
+			So(account.AccountID, ShouldEqual, v.GetInt64())
+			So(account.Role, ShouldEqual, v.GetString())
+			So(account.Name, ShouldEqual, v.GetString())
+			So(account.ChatworkID, ShouldEqual, v.GetString())
+			So(account.OrganizationID, ShouldEqual, v.GetInt64())
+			So(account.OrganizationName, ShouldEqual, v.GetString())
+			So(account.Department, ShouldEqual, v.GetString())
+			So(account.AvatarImageURL, ShouldEqual, "https://"+v.GetString())
+		})
+
 		Convey("MembersRaw", func() {
 			stub := &stubHTTP{}
-			stub.GetByte = []byte("")
+			stub.GetByte = []byte(correctJSON)
 			client.connection = stub
 
 			b, _ := client.MembersRaw(42)
-			So(string(b), ShouldEqual, "")
+			So(string(b), ShouldEqual, correctJSON)
 			So(stub.GetCount, ShouldEqual, 1)
 			So(stub.GetEndPoint, ShouldEqual, "rooms/42/members")
 		})
